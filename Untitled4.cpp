b293f4b5f249601a06ecb9723bc979a177db3c72
MUHAMMAD ASIM ALYAS <135734492+asimalyas@users.noreply.github.com>
2023-06-14 14:53:21 +0500

    fibnochi sequence
    fibnochi sequence in function

--- Untitled4.cpp
+++ Untitled4.cpp
@@ -1,1 +1,16 @@
+#include<stdio.h>
+int fib(int n){
+	if(n==0||n==1){
+		return n;
+	}
+	else{
+		return fib(n-1)+fib(n-2);
+	}
+}
+main(){
+for(int i=1;i<=10;i++){
 
+	int result = fib(i);
+	printf("\n# %d fibnochi number =  %d",i,result);
+}
+}
